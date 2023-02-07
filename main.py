3658f44643109c6babbd8bbad3beaaaed5207ab0
superdev0410 <superdev0410@protonmail.com>
2023-02-07 13:31:07 -0800

    parse command line arguments

--- main.py
+++ main.py
@@ -6,13 +6,37 @@ import argparse
 import os
 
 
-def parse_args() -> argparse.Namespace:
+def get_args() -> argparse.Namespace:
     """parse command line arguments
 
     Returns:
         argparse.Namespace: parsed arguments
     """
-    pass
+    
+    # define parsers and subparsers
+    parser = argparse.ArgumentParser(description="Choose and add files to source folder.")
+    subparsers = parser.add_subparsers(dest="command")
+    addparser = subparsers.add_parser("add", help="add files to source folder")
+    chooseparser = subparsers.add_parser("choose", help="choose files and copy to anohter folder")
+
+    # add arguments to addparser
+    addparser.add_argument("files", nargs="+", help="list of filepaths to add")
+    
+
+    # add arguments to chooseparser
+    chooseparser.add_argument(
+        "-n", "--num", 
+        type=int, 
+        help="number of files to choose", 
+        default=50
+    )
+    chooseparser.add_argument(
+        "-d", "--destination",
+        help="destination folder path",
+        default="E:\MyProgram\Screenshot\Source"
+    )
+    
+    return parser.parse_args()
 
 
 def add(filepaths: list):
@@ -21,7 +45,7 @@ def add(filepaths: list):
     Args:
         filepaths (list): list of files
     """
-    pass
+    print("add")
 
 
 def choose(num: int, destination: str):
@@ -31,14 +55,23 @@ def choose(num: int, destination: str):
         num (int): number of files to choose
         destination (str): destination folder path
     """
-    pass
+    print("choose")
 
 
 def main():
     """
     main function
     """
-    pass
+    
+    # parse command line arguments
+    args = get_args()
+    if args.command == "add":
+        # start add command
+        add(args.files)
+
+    elif args.command == "choose":
+        # start choose command
+        choose(args.num, args.destination)
 
 
 if __name__ == "__main__":
