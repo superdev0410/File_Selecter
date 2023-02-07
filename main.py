56fbb86a93e8dfa2dbb14e3d2d99b686e47e8f5a
superdev0410 <superdev0410@protonmail.com>
2023-02-07 13:02:18 -0800

    basic structure

---
+++ main.py
@@ -0,0 +1,45 @@
+"""
+main module
+"""
+
+import argparse
+import os
+
+
+def parse_args() -> argparse.Namespace:
+    """parse command line arguments
+
+    Returns:
+        argparse.Namespace: parsed arguments
+    """
+    pass
+
+
+def add(filepaths: list):
+    """add files to source folder
+
+    Args:
+        filepaths (list): list of files
+    """
+    pass
+
+
+def choose(num: int, destination: str):
+    """randomly choose files and copy to destination folder
+
+    Args:
+        num (int): number of files to choose
+        destination (str): destination folder path
+    """
+    pass
+
+
+def main():
+    """
+    main function
+    """
+    pass
+
+
+if __name__ == "__main__":
+    main()
